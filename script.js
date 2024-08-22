0876c90c6a6ebf000586dafec636331c1b232920
baronejavier <74462330+baronejavier@users.noreply.github.com>
2024-08-22 20:02:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,158 @@
-// Datos de ejemplo para las propiedades
-const propiedades = [
+  const propiedades = [
+    {
+        nombre: "Loteo Club de Campo Santa Cecilia",
+        localizacion: "Candelaria",
+        categoria: "Terrenos",
+        precio: 37000,
+        imagenes: [
+            "img/santa cecilia1.jpg",
+            "img/santa cecilia2.jpg",
+            "img/santa cecilia3.jpg"
+        ],
+        descripcion: `Lote de 20x50m mensurado y amojonado.
+                          Barrio listo para construir, con posibilidad de escritura.
+                          Tiene una playa con muelle y sector náutico a orillas del río Paraná, espacios verdes, dos salones de usos múltiples, cancha de tenis y de fútbol.
+                          Las calles están asfaltadas, y tiene todos los servicios subterráneos (luz, agua y cloaca).
+                          Pensado como un magnífico lugar para vivir o tener una casa de fin de semana, el proyecto tiene grandes espacios libres, una espléndida arboleda tan solo a 30 km de Posadas. Está ubicado en el corazón de la Cuenca de la Plata en una zona con un gran crecimiento demográfico y auge de consolidación de barrios privados.`
+    },
+
+    {
+        nombre: "Loteo La Candelaria",
+        localizacion: "Candelaria",
+        categoria: "Terrenos",
+        precio: 27000,
+        imagenes: [
+            "img/la candelaria1.png",
+            "img/la candelaria2.jpg",
+            "img/la candelaria3.jpg"
+        ],
+        descripcion: `Lote de 23x32m.
+Barrio Cerrado con Portal de Ingreso y Servicio de Seguridad 24hs.
+Tendido eléctrico y agua.
+Cordón Cuneta.
+Título de Propiedad.
+Cuenta con una espléndida vista al Arroyo Garupá, y la Reserva Urutaú. Lugar ideal para vivir o tener una casa de fin de semana. A 20 minutos de Posadas.`
+    },
+
+    {
+        nombre: "Loteo La Agustina",
+        localizacion: "Garupá",
+        categoria: "Terrenos",
+        precio: 15000,
+        imagenes: [
+            "img/la agustina1.jpg",
+            "img/la agustina2.jpg",
+            "img/la agustina3.jpg"
+        ],
+        descripcion: `Lotes de 10x50m sobre Ruta Nacional 105. Barrio Santa Inés.
+Contará con Portal de Entrada con casilla de seguridad.
+Contará con Club House para reuniones, cumpleaños, agasajos.
+Especial comercio o vivienda familiar.`
+    },
+
+    {
+        nombre: "Loteo Urutaú Country",
+        localizacion: "Garupá",
+        categoria: "Terrenos",
+        precio: 10000,
+        imagenes: [
+            "img/urutau1.jpg",
+            "img/urutau2.jpg",
+            "img/urutau3.jpg"
+        ],
+        descripcion: `Lotes de 15x45m en barrio privado Urutaú Country, Barrio Santa Inés.
+Predio de 3 Ha, con una excelente vista al Arroyo Garupá y la reserva natural Urutaú. 
+Calle privada con portal de ingreso. 
+Casilla de seguridad las 24 hs.
+Contará con piscina con solárium y salón de eventos. Idel para casas de fin de semana. 
+Elegí el encanto natural, la propuesta de vivir a 20' de la ciudad, rodeado de verde y naturaleza.`
+    },
+
+    {
+        nombre: "Casa Quinta",
+        localizacion: "Garupá",
+        categoria: "Terrenos",
+        precio: 100000,
+        imagenes: [
+            "img/casa quinta1.jpg",
+            "img/casa quinta2.jpg",
+            "img/casa quinta3.jpg"
+        ],
+        descripcion: `Ubicada en Santa Inés, frente al arroyo Garupá. A 3 Km de la Ruta Nacional 105. Total de 3.2 Hectáreas con pinos y variedad de árboles frutales. Cabaña amoblada, con 2 habitaciones, cocina comedor, baño. Sótano. Galería de 2m que rodea toda la casa.
+Superficie cubierta 180 m2.
+Piscina de 10x5m.`
+    },
+
+    {
+        nombre: "Terreno en Miguel Lanús",
+        localizacion: "Posadas",
+        categoria: "Terrenos",
+        precio: 15000,
+        imagenes: [
+            "img/miguel lanus1.jpg",
+            "img/miguel lanus2.jpg",
+            "img/miguel lanus3.jpg"
+        ],
+        descripcion: `Terreno en Zona Universitaria Campus UNaM - Misiones.
+Oportunidad única para inversionistas y constructores.
+Se vende terreno en zona universitaria de Posadas, Misiones, a minutos del centro.
+Ubicación privilegiada Zona Miguel Lanús.
+Escuela de Cadetes de la Policía.
+Acceso Sur y Playa Costa Sur.
+Cercanía al Hospital y Juzgado.
+Zona de alta demanda y crecimiento.
+Terreno de 10x40m apto para construcción.
+Servicios básicos disponibles (agua, electricidad, red cloacal).
+Acceso fácil y seguro.
+Oportunidad de inversión,
+ideal viviendas unifamiliares o departamentos para alquiler.
+Desarrollar un proyecto comercial o de servicios.
+Zona con alto potencial de crecimiento.`
+    },
+
+    {
+        nombre: "Vivienda Familiar Barrio Judicial",
+        localizacion: "Posadas",
+        categoria: "Casas",
+        precio: 100000,
+        imagenes: [
+            "img/barrio judicial1.jpg",
+            "img/barrio judicial2.jpg",
+            "img/barrio judicial3.jpg"
+        ],
+        descripcion:
+`Esta impresionante residencia moderna combina el lujo con la comodidad, ofreciéndote el hogar perfecto que siempre deseaste.
+Disfruta de cada día en esta hermosa casa, con una hermosa piscina para relajarte y refrescarte en los días calurosos.
+Ubicada en plena ciudad, tendrás todo lo que necesitas a tu alcance.
+Barrio judicial. Monseñor de Andrea & Centenario.
+Medidas 10x25m.
+<p></p>
+• 4 habitaciones 
+• Living 
+• Cocina 
+• Comedor 
+• Lavadero 
+• Garage con techo 
+• Hall de Entrada
+• Patio abierto con parrilla y pileta 
+• Entrada cerrado con portón eléctrico 
+• Termotanque`
+    },
+
     {
         nombre: "Departamento en Altos de Posadas",
         localizacion: "Posadas",
         categoria: "Departamentos",
         precio: 165000,
         imagenes: [
-            "img/depto1.jpg",
-            "img/depto2.jpg",
-            "img/depto3.jpg"
+            "img/altos de posadas1.jpg",
+            "img/altos de posadas2.jpg",
+            "img/altos de posadas3.jpg"
         ],
-        descripcion: "Departamento en Posadas con vista al río."
+        descripcion: `Centro de Posadas. Esquina 25 de Mayo y Belgrano. 2 habitaciones. Cochera. Primer nivel piso 10. Vista increíble al río Paraná.`
     },
-    {
-        nombre: "Departamento en Altos de Posadas",
-        localizacion: "Posadas",
-        categoria: "Departamentos",
-        precio: 165000,
-        imagenes: [
-            "img/depto1.jpg",
-            "img/depto2.jpg",
-            "img/depto3.jpg"
-        ],
-        descripcion: "Departamento en Posadas con vista al río."
-    },
-    {
-        nombre: "Departamento en Altos de Posadas",
-        localizacion: "Posadas",
-        categoria: "Departamentos",
-        precio: 165000,
-        imagenes: [
-            "img/depto1.jpg",
-            "img/depto2.jpg",
-            "img/depto3.jpg"
-        ],
-        descripcion: "Departamento en Posadas con vista al río."
-    },
+    
+
     
 ];
 
@@ -66,7 +183,7 @@ function mostrarPropiedades() {
 
     // Limpiar lista de propiedades y agregar las filtradas
     listaPropiedades.innerHTML = "";
-    propiedadesFiltradas.forEach(prop => {
+    propiedadesFiltradas.forEach((prop, index) => {
         const propiedadDiv = document.createElement("div");
         propiedadDiv.classList.add("propiedad");
 
@@ -87,6 +204,7 @@ function mostrarPropiedades() {
             <p>Localización: ${prop.localizacion}</p>
             <p>Categoria: ${prop.categoria}</p>
             <p>Precio: U$D ${prop.precio}</p>
+            <button id="btnWhatsapp-${index}">Agenda tu visita</button>
         `;
 
         // Agregar el contenedor de imágenes a la propiedad
@@ -94,6 +212,15 @@ function mostrarPropiedades() {
 
         // Agregar la propiedad al contenedor principal
         listaPropiedades.appendChild(propiedadDiv);
+
+        // Añadir evento al botón de WhatsApp
+        const btnWhatsapp = document.getElementById(`btnWhatsapp-${index}`);
+        btnWhatsapp.addEventListener('click', () => {
+            const mensaje = `Hola Vanesa, quisiera más información sobre ${prop.nombre}`;
+            const telefono = '3765383612'; // Número de teléfono de destino
+            const url = `https://api.whatsapp.com/send?phone=${telefono}&text=${encodeURIComponent(mensaje)}`;
+            window.open(url);
+        });
     });
 }
 
@@ -104,3 +231,127 @@ precioFiltro.addEventListener("input", mostrarPropiedades);
 
 // Mostrar todas las propiedades al cargar la página
 mostrarPropiedades();
+
+const construcciones = [
+    {
+        nombre: "Cabaña de Eucalipto",
+        descripcion: `Construcción desde la base.
+Cabañero Eucalipto.
+Frente inglés o cabañero de 1 pulgada. 
+Primera calidad.
+Estructura de la casa de tirantes de 2*3/2*4/2*5.
+Baño de mampostería de ladrillo del 12 hueco. Revestimiento machimbre de PVC y vinilo. 
+Artefactos Ferrum, Vanitory.
+Cocina, Sala comedor.
+Cielorraso a la vista.
+Techo de chapa y aislantes térmicos.
+Puertas de madera.
+Ventanas 1*1 madera/aluminio. 
+Instalación de luz completa. 
+Instalación de agua fría y caliente.
+
+`,
+        precioPorMetroCuadrado: 550,
+        imagenes: [
+            "img/cabaña eucalipto1.jpg",
+            "img/cabaña eucalipto2.jpg",
+            "img/cabaña eucalipto3.jpg"
+        ]
+    },
+
+    {
+        nombre: "Cabaña de Quebracho Blanco",
+        descripcion: `Construcción desde la base.
+Frente inglés o cabañero de 1 pulgada. 
+Primera calidad.
+Estructura de la casa de tirantes de 2*3/2*4/2*5.
+Baño de mampostería de ladrillo del 12 hueco. Revestimiento machimbre de PVC y vinilo. 
+Artefactos Ferrum, Vanitory.
+Cocina, Sala comedor.
+Cielorraso a la vista.
+Techo de chapa y aislantes térmicos.
+Puertas de madera.
+Ventanas 1*1 madera/aluminio. 
+Instalación de luz completa. 
+Instalación de agua fría y caliente.
+
+`,
+        precioPorMetroCuadrado: 670,
+        imagenes: [
+            "img/quebracho1.jpg",
+            "img/quebracho2.jpg",
+            "img/quebracho3.jpg"
+        ]
+    },
+
+    {
+        nombre: "Cabaña de Tronco de Eucalipto",
+        descripcion: `Construcción desde la base.
+Frente inglés o cabañero de 1 pulgada. 
+Primera calidad.
+Baño de mampostería de ladrillo del 12 hueco. Revestimiento machimbre de PVC y vinilo. 
+Artefactos Ferrum, Vanitory.
+Cocina, Sala comedor.
+Cielorraso a la vista.
+Techo de chapa y aislantes térmicos.
+Puertas de madera.
+Ventanas 1*1 madera/aluminio. 
+Instalación de luz completa. 
+Instalación de agua fría y caliente.
+
+`,
+        precioPorMetroCuadrado: 630,
+        imagenes: [
+            "img/tronco1.jpg",
+            "img/tronco2.jpg",
+            "img/tronco3.jpg"
+        ]
+    },
+
+];
+
+const listaConstruccion = document.getElementById("lista-construccion");
+
+function mostrarConstrucciones() {
+    listaConstruccion.innerHTML = "";
+    construcciones.forEach((construccion, index) => {
+        const construccionDiv = document.createElement("div");
+        construccionDiv.classList.add("construccion");
+
+        // Crear un contenedor para las imágenes
+        const imagenesDiv = document.createElement("div");
+        imagenesDiv.classList.add("imagenes");
+        construccion.imagenes.forEach(imgSrc => {
+            const img = document.createElement("img");
+            img.src = imgSrc;
+            img.alt = construccion.nombre;
+            img.style.width = "100%"; // Ajusta el estilo según sea necesario
+            imagenesDiv.appendChild(img);
+        });
+
+        construccionDiv.innerHTML = `
+            <h3>${construccion.nombre}</h3>
+            <p>${construccion.descripcion}</p>
+            <p>Precio por m²: U$D ${construccion.precioPorMetroCuadrado}</p>
+            <button id="btnWhatsappConstruccion-${index}">Solicitar presupuesto</button>
+        `;
+
+        // Agregar el contenedor de imágenes a la construcción
+        construccionDiv.appendChild(imagenesDiv);
+
+        // Agregar la construcción al contenedor principal
+        listaConstruccion.appendChild(construccionDiv);
+
+        // Añadir evento al botón de WhatsApp
+        const btnWhatsapp = document.getElementById(`btnWhatsappConstruccion-${index}`);
+        btnWhatsapp.addEventListener('click', () => {
+            const mensaje = `Hola, quisiera más información sobre ${construccion.nombre}`;
+            const telefono = '3765383612'; // Número de teléfono de destino
+            const url = `https://api.whatsapp.com/send?phone=${telefono}&text=${encodeURIComponent(mensaje)}`;
+            window.open(url);
+        });
+    });
+}
+
+// Mostrar todas las construcciones al cargar la página
+mostrarConstrucciones();
